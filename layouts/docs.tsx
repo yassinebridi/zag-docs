951d7998429a99c11f3a0e3187dc8d96575a0d15
Segun Adebayo <joseshegs@gmail.com>
2021-12-20 08:46:07 +0100

    chore: update

--- layouts/docs.tsx
+++ layouts/docs.tsx
@@ -9,20 +9,21 @@ export default function DocsLayout({ children, toc }) {
 
   return (
     <chakra.main pt="6.5rem" px={{ base: "16", md: "8" }}>
-      <Box maxW="8xl" mx="auto" position="relative">
+      <Box maxW="7xl" mx="auto" position="relative">
         <Box
           display="flex"
           flexDirection="row"
-          gap="48px"
+          gap={{ md: "xl" }}
           alignItems="flex-start"
         >
-          <Box width="14rem">
+          <Box width="14rem" display={{ base: "none", md: "block" }}>
             <Sidebar />
           </Box>
           <Box flex="1" mt="12" mb="40" px="8">
             {children}
           </Box>
           <Box
+            display={{ base: "none", lg: "block" }}
             width="14rem"
             position="sticky"
             top="xl"
