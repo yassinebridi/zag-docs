1c4db2cdcbf1def08cd8b30b8b9e96ac90163ba3
Segun Adebayo <joseshegs@gmail.com>
2022-03-23 19:16:07 +0400

    chore: add ts usage

--- sidebar.config.ts
+++ sidebar.config.ts
@@ -36,6 +36,7 @@ const sidebar: Record<"docs", SidebarItem[]> = {
         { type: "doc", label: "Introduction", id: "introduction" },
         { type: "doc", label: "Installation", id: "getting-started" },
         { type: "doc", label: "State machine", id: "whats-a-machine" },
+        { type: "doc", label: "TypeScript usage", id: "typescript-usage" },
       ],
     },
     {
