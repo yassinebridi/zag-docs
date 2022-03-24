3dc99d1c4c9fc3af9f6999463f1fb4e430bafe04
Segun Adebayo <joseshegs@gmail.com>
2022-03-24 13:49:34 +0400

    chore: extract pagination component

--- components/mdx-footer.tsx
+++ components/mdx-footer.tsx
@@ -1,49 +1,19 @@
-import { Box, Flex, HStack } from "@chakra-ui/layout"
+import { Box } from "@chakra-ui/layout"
 import { chakra } from "@chakra-ui/system"
-import { usePagination } from "lib/use-pagination"
-import Link from "next/link"
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi"
+import siteConfig from "site.config"
+import { Pagination } from "./pagination"
 
 export function MdxFooter() {
-  const { prev, next } = usePagination()
   return (
-    <chakra.footer fontSize="sm" mt="12">
-      <Flex
-        align="center"
-        justify="space-between"
-        mb="10"
-        color="gray.700"
-        fontWeight="semibold"
-      >
-        {prev ? (
-          <Link href={prev.url} passHref>
-            <HStack as="a" spacing="3" rel="prev">
-              <HiChevronLeft />
-              <span>{prev.label}</span>
-            </HStack>
-          </Link>
-        ) : (
-          <div className="pagination__empty" />
-        )}
-        {next ? (
-          <Link href={next.url} passHref>
-            <HStack as="a" spacing="3" rel="next">
-              <span>{next.label}</span>
-              <HiChevronRight />
-            </HStack>
-          </Link>
-        ) : (
-          <div className="pagination__empty" />
-        )}
-      </Flex>
-
+    <chakra.footer mt="12">
+      <Pagination />
       <Box
         pt="10"
         borderTopWidth="1px"
         display={{ sm: "flex" }}
         justifyContent="space-between"
       >
-        <Box mb={{ base: "6", sm: "0" }}>
+        <Box fontSize="sm" mb={{ base: "6", sm: "0" }}>
           <p>
             Proudly made in
             <chakra.span role="img" aria-label="Nigeria" mx="2">
@@ -52,12 +22,15 @@ export function MdxFooter() {
             by Segun Adebayo
           </p>
         </Box>
-        <a href="#">Edit this page on GitHub</a>
       </Box>
 
-      <Box fontSize="xs" mt="4" mb="28" opacity={0.5}>
-        Copyright © {new Date().getFullYear()}
-      </Box>
+      <Box
+        fontSize="sm"
+        mt="4"
+        mb="28"
+        opacity={0.5}
+        dangerouslySetInnerHTML={{ __html: siteConfig.copyright }}
+      />
     </chakra.footer>
   )
 }
